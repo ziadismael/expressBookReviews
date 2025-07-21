92856850ad3cb2c90720533d230ffff4b0091710
Ziad Ismael <ziad.ismael@icloud.com>
2025-07-21 07:52:26 +0300

    Added New Functionalites for Registering, Logging and Authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,13 +10,47 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let matches = users.filter((user)=>{
+      return (username === user.username && password === user.password);
+    });
+
+    if(matches.length > 0){
+      return true;
+    }
+    else{
+      return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+      const username = req.body.username;
+      const password = req.body.password;
+
+      // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    //Authentice User
+    if(authenticatedUser(username,password)) {
+        let accessToken = jwt.sign(
+          {data:password},
+          'access',
+          {expiresIn: 60 * 60}
+        );
+        
+        req.session.authorization = {
+          accessToken, username
+        }
+
+        return res.status(200).json({message:"User has logged in"});
+    }
+    else{
+      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+    
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,23 +4,58 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+
+  if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(username && password){
+    if(!doesExist(username)){
+      users.push({"username" : username, "password" : password});
+      return res.status(200).json({message: "Users Created Successfully!"});
+    }
+    else{
+      return res.status(404).json({message: "Username Already Exists"});
+    }
+  }
+  else{
+    return res.status(404).json({message : "Re-enter the username and password."})
+  }
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(booksdb, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let ISBN = req.params.isbn;
+  let matches = books.filter((book) => {
+    return (book.isbn === ISBN);
+  });
+  if (matches > 0){
+    return res.status(200).json(matches[0]);
+  }
+  else{
+    return res.status(404).json({message : "No such ISBN number."});
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
